5eca4e7fea1117b483373dd4b5b3c82df868c1a9
jamesthe500 <jamesddaniels@gmail.com>
2015-10-15 20:42:02 -0500

    Completes project mostly.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-This is my solution for the take-home Identifix code challenge.<br>
-After downloading the zip, don't forget to run an "npm install" to get bootstrap and jquery in their right places.<br>
+# This is my solution for the take-home Identifix code challenge.
+*After downloading and extracting the zip, run an "npm install" to get bootstrap and jquery in their right places.*
+
+## The challenge was to create a simple app that uses the CarQuery API to run a search of car model trims. After perusing the documentation and running some experiments, I got the API to return data with a keyword search. I built the app from there,
 
 One challenge was figuring out how to best calculate horsepower, since it wasn't one of the parameters given by CarQuery. In my search through Wikipedia and other such sites, I found algebra to convert Newton-meters to HP, but RPMs were needed and they weren't necessarily defined. I eventually landed on a <a href="http://www.caranddriver.com/columns/larry-webster-horsepower-confusion-and-resolution-column">Car and Driver</a> article which went in a different direction, but did teach me about the unit, Pferdestärke (PS) which is provided by CarQuery. One PS is equal to 0.9863 HP. I applied this to the Chevrolet Corvette from the example, which has a rating of 650HP, and came up with 641hp, quite different from the example of 449hp. I chalked this up to the example using a different source, which was more or less confirmed by a search for Chevrolet's published statistics.
--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
+var failNumber = 0;
+var failMessage = "";
+
 $(document).ready(function() {
     $('#submitbtn').on('click',( function(){
         var searchTerms= $("#searchfield").val().replace("'","");
 
-        $.getJSON("http://www.carqueryapi.com/api/0.3/?callback=?", {cmd:"getTrims", keyword:searchTerms}, function(data) {
+        $.getJSON("http://www.carqueryapi.com/api/0.3/?callback=?", {cmd:"getTrims", keyword:searchTerms}).done(function(data) {
             $('#results').empty();
 
             var trims = data.Trims;
             if (trims.length == 0){
-                $('#results').append("<div class='noResults'>Sorry, that search returned no results. Please check your spelling, make sure you've included all the necessary terms, and try again.</div>");
+                $('#results').append("<div class='trimResult trim'>Sorry, that search returned no results. Please check your spelling, make sure you've included all the necessary terms, and try again.</div>");
             }
 
             for(i=0; i < trims.length; i++) {
@@ -32,27 +35,45 @@ $(document).ready(function() {
                         liters = (Math.round(trims[i].model_engine_cc / 100) / 10) + "L ";
                     }
 
-                    engineResult = "<div class='engine resultLine'>Engine: " + liters + engineType + "</div>"
+                    engineResult = "<div class='engine'>Engine: " + liters + engineType + "</div>"
                 }
 
                 var trimResult = "";
                 if (returnedTrim !== "" && returnedTrim !== null){
-                    trimResult = "<div class='trim resultLine'>Trim: " + returnedTrim + "</div>";
+                    trimResult = "<div class='trim'>" + returnedTrim + "</div>";
                 }
 
                 var transmissionResult = "";
                 if (returnedTrans !== "" && returnedTrans !== null){
-                    transmissionResult = "<div class='transmission resultLine'>Transmission: " + returnedTrans + "</div>";
+                    transmissionResult = "<div class='transmission'>Transmission: " + returnedTrans + "</div>";
                 }
 
                 var powerResult = "";
                 if (returnedPS !== "" && returnedPS !== null){
-                    transmissionResult = "<div class='transmission resultLine'>Maximum horsepower: " + Math.round(returnedPS * 0.9863) + "hp</div>";
+                    transmissionResult = "<div class='transmission'>Maximum horsepower: " + Math.round(returnedPS * 0.9863) + "hp</div>";
                 }
 
-                $('#results').append("<div class='trimResult resultLine'><div class='headline'>" + trims[i].model_year + " " + trims[i].make_display + " " + trims[i].model_name + "</div>" + trimResult + engineResult + transmissionResult + powerResult + "</div>")
+                $('#results').append("<div class='trimResult'><div class='headline'>" + trims[i].model_year + " " + trims[i].make_display + " " + trims[i].model_name + "</div>" + trimResult + engineResult + transmissionResult + powerResult + "</div>")
 
             }
+            failNumber = 0;
+        }).fail(function(){
+            $('#results').empty();
+            switch (failNumber){
+                case 0:
+                    failMessage = "Sorry, something went wrong with your search. Please try again.";
+                    break;
+                case 1:
+                    failMessage = "Perhaps the server is down. Try even later.";
+                    break;
+                case 2:
+                    failMessage = "You are persistent! If at first you don't succeed...";
+                    break;
+                default:
+                    failMessage = "The problem is even more persistent than you are, it seems. Please contact the site administrator for assistance.";
+            }
+            $('#results').append("<div class='trimResult trim'>" + failMessage + "</div>");
+           failNumber ++;
         });
     }));
 }); // end of doc ready
